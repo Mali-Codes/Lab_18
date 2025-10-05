ace4420887636d586564f4f6f514954de4537349
Malakai <poncemalakai@gmail.com>
2025-10-04 20:08:54 -0700

    making the asking part

--- Lab_18.cpp
+++ Lab_18.cpp
@@ -1,4 +1,27 @@
+// Malakai Ponce | October 04, 2025 | COMSC 210
+// Lab 18
+
 #include <iostream>
 #include <string>
 #include <iomanip>
 using namespace std;
+
+struct ReviewNode {
+    double rating;
+    string comment;
+    reviewNode* next;
+};
+
+
+
+
+int main() {
+
+//Make the list
+
+cout << "[1] add to the front";
+cout << "[2] add to the back";
+
+
+
+}
